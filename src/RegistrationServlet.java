9e7dfaf3af05d2c06534d07537c26c7a6261bed5
Kay Hickey <c23434334@mytudublin.ie>
2025-10-29 14:05:27 +0000

    second commit

--- src/RegistrationServlet.java
+++ src/RegistrationServlet.java
@@ -1,3 +1,31 @@
-public class RegistrationServlet {
+package ;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class RegistrationServlet extends HttpServlet{
 	
-}
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException{
+		
+		
+	String gamerTag = request.getParameter("Gamer Tag");
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<html><body>Thanks, your gamer tag is: "
+		+gamerTag+"</body></html>");
+		
+	
+	String password = request.getParameter("Password");
+	
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+	}
+	}
