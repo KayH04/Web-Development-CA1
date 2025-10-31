2f76f3fd809eb01adf6f5fe39aa0ce9f27542019
Kay Hickey <c23434334@mytudublin.ie>
2025-10-31 17:16:05 +0000

    fifth commit

---
+++ src/LoginServlet.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+public class LoginServlet {
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+		
+		String gamerTag = request.getParameter("gamerTag");
+		String password = request.getParameter("password");
+		int points = request.getParameter("points");
+		
+		Connection connection;
+		
+		if(password.equals(password) && gamerTag.equals(gamerTag)) {
+			PreparedStatement pstmt = connection.prepareStatement
+					("SELECT password, gameTag, points FROM users");
+		}
+				
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+	}
+
+}
