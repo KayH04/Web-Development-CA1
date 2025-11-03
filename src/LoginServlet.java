f9db1d7909de8c005bbe3258c3f6b78a9eab44fa
Kay Hickey <c23434334@mytudublin.ie>
2025-11-03 13:53:34 +0000

    seventh commit

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -4,45 +4,80 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
-public class LoginServlet {
+public class LoginServlet extends HttpServlet{
+	private Connection connection;
+	
+	
+	public void init() throws ServletException{
+		//CONNECT TO DATABASE "gamingPortal"
+		try {
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/gamingPortal", "root", "rootroot1");
+		} catch (SQLException e) {
+			throw new ServletException("Database connection error", e);
+		}
+	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-		throws ServletException, IOException {
+			throws ServletException, IOException {
 		
 		String gamerTag = request.getParameter("gamerTag");
 		String password = request.getParameter("password");
-		int points = request.getParameter("points");
 		
-		Connection connection;
+		PreparedStatement pstmt = null;
+		ResultSet result = null;
 		
-		if(password.equals(password) && gamerTag.equals(gamerTag)) {
-			PreparedStatement pstmt = connection.prepareStatement
-					("SELECT password, gameTag, points FROM users");
+		try {
+			String sql = "SELECT password, gamerTag, points FROM users WHERE gamerTag = ?";
+			pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1,  gamerTag);
+			result = pstmt.executeQuery();
 			
-			pstmt.setString(1, gamerTag);
-			pstmt.setString(2, password);
-			ResultSet result = pstmt.executeQuery();
-			
-		if(result.next()) {
-			HttpSession session = request.getSession();
-			session.setAttribute("gamerTag", gamerTag);
-			session.setAttribute("points", result.getInt("points"));
-			response.sendRedirect("index.html");
-		}
-		}
+			if(result.next()) {
+				String correctPassword = result.getString("password");
 				
+				if(password.equals(correctPassword)) {
+				pstmt.setString(1, gamerTag);
+				pstmt.setString(2, password);
+				result = pstmt.executeQuery();
+			
+				HttpSession session = request.getSession();
+				session.setAttribute("gamerTag", gamerTag);
+				session.setAttribute("points", result.getInt("points"));
+				response.sendRedirect("index.html");
+				
+				} else {
+				
+				System.out.println("Incorrect password, please try again.");
+				
+				}
+				
+			} else {
+				
+			}
+				
+			
+		} catch(SQLException e) {
+			
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			out.println("<html><body>");
+			out.println("<p>Invalid credentials. Please try again. </p>");
+			out.println("<a href='login.html'>Return to Login</a>");
+			out.println("</body></html>");
 		
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
 		
-		
+	
+		}
 	}
-
 }
--- src/PointsServlet.java
+++ src/PointsServlet.java
@@ -1,4 +1,67 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class PointsServlet {
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+
+public class PointsServlet extends HttpServlet{
+	
+	private Connection connection;
+	
+		public void init() throws ServletException{
+			try {
+				connection = DriverManager.getConnection(
+						"jdbc:mysql://localhost:3306/gamingPortal", "root", "rootroot1");
+			} catch (SQLException e) {
+				throw new ServletException("Database connection error", e);
+			
+		
+			}
+		}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException{
+		
+		HttpSession session =request.getSession(false);
+		if(session == null || session.getAttribute("gamerTag")== null) {
+			response.sendRedirect("login.html");
+			return;
+		}
+		String gamerTag;
+		String password;
+		int points;
+		int purchaseAmount;
+		
+		
+		
+		PreparedStatement pstmt = connection.prepareStatement(
+				"SELECT points, gamerTag FROM users");
+		
+	if(points > purchaseAmount) {
+		
+		PreparedStatement update = connection.prepareStatement(
+				"UPDATE users SET points =?");
+	} else {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Error! the purchase amount will send your balance below 0 points");
+	}
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>");
+		out.println("<p>Welcome back"+ gamerTag + "</p>");
+		out.println("<p>You have :<b>" + points + "</b> points</p>");
+		
+	}
+		
 }
